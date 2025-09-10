c82a03d3dc12706a399ab88f3754d988786ea4b7
OzzeY72 <nabacap72@gmail.com>
2025-09-10 14:58:18 +0200

    Create frontend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -7,7 +7,10 @@ import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      'eslint.config.mjs',
+      'test/app.e2e-spec.ts'
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
